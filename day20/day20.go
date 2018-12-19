2ae5ee3738b8821c3290c9ca31027d26ac44728c
Jacob Wirth <xthexder@live.com>
2018-12-19 18:43:47 -0500

    Starting attempt at Day 20

---
+++ day20/day20.go
@@ -0,0 +1,175 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+var minx, maxx int = 2000, 0
+var miny, maxy int = 2000, 0
+
+func minmax(x, y int) {
+	if x < minx {
+		minx = x
+	}
+	if x > maxx {
+		maxx = x
+	}
+	if y < miny {
+		miny = y
+	}
+	if y > maxy {
+		maxy = y
+	}
+}
+
+func parseRegex(regex []byte) *Node {
+	if len(regex) == 0 {
+		// fmt.Println("Blank")
+		return &Node{}
+	}
+	// fmt.Println("Start", string(regex))
+	node := &Node{}
+	nest := 0
+	sectionStart := 0
+	for i, b := range regex {
+		switch b {
+		case '(':
+			nest++
+			if nest == 1 {
+				node.value = regex[:i]
+				sectionStart = i + 1
+				// fmt.Println("(: ", string(regex[:i]))
+			}
+		case '|':
+			if nest == 1 {
+				// fmt.Println("|: ", string(regex[sectionStart:i]))
+				node.children = append(node.children, parseRegex(regex[sectionStart:i]))
+				sectionStart = i + 1
+			}
+		case ')':
+			if nest == 1 {
+				// fmt.Println("): ", string(regex[sectionStart:i]))
+				node.children = append(node.children, parseRegex(regex[sectionStart:i]))
+				if i < len(regex)-1 {
+					node.next = parseRegex(regex[i+1:])
+				}
+				// fmt.Println("End2", string(regex))
+				return node
+			}
+			nest--
+		}
+	}
+	node.value = regex
+	// fmt.Println("End1", string(regex))
+	return node
+}
+
+type Node struct {
+	value    []byte
+	children []*Node
+	next     *Node
+	cache    map[complex64][]complex64
+}
+
+func (n *Node) String() string {
+	if n == nil {
+		return ""
+	}
+
+	out := " [" + string(n.value)
+	if len(n.children) > 1 {
+		out += "("
+	}
+	for i, c := range n.children {
+		if i > 0 {
+			out += "|"
+		}
+		out += c.String()
+	}
+	if len(n.children) > 1 {
+		out += ")"
+	}
+	out += n.next.String()
+	out += "] "
+	return out
+}
+
+func (n *Node) traverse(pos complex64, board [][]byte) []complex64 {
+	if n == nil {
+		return []complex64{pos}
+	}
+	if n.cache == nil {
+		n.cache = make(map[complex64][]complex64)
+	} else if result, ok := n.cache[pos]; ok {
+		return result
+	}
+	inPos := pos
+
+	for _, v := range n.value {
+		switch v {
+		case 'N':
+			board[int(imag(pos)*2)-1][int(real(pos)*2)] = '-'
+			pos += -1i
+		case 'S':
+			board[int(imag(pos)*2)+1][int(real(pos)*2)] = '-'
+			pos += 1i
+		case 'E':
+			board[int(imag(pos)*2)][int(real(pos)*2)+1] = '|'
+			pos += 1
+		case 'W':
+			board[int(imag(pos)*2)][int(real(pos)*2)-1] = '|'
+			pos += -1
+		default:
+			panic("Unknown direction")
+		}
+		board[int(imag(pos)*2)][int(real(pos)*2)] = '.'
+		minmax(int(real(pos)*2), int(imag(pos)*2))
+	}
+	if len(n.children) == 0 {
+		n.cache[inPos] = n.next.traverse(pos, board)
+	} else {
+		for _, child := range n.children {
+			posList := child.traverse(pos, board)
+			for _, afterPos := range posList {
+				n.cache[inPos] = append(n.cache[inPos], n.next.traverse(afterPos, board)...)
+			}
+		}
+		// fmt.Println(len(n.cache[inPos]))
+	}
+	return n.cache[inPos]
+}
+
+func main() {
+	var regex []byte
+	board := make([][]byte, 2000)
+	for y := 0; y < len(board); y++ {
+		board[y] = make([]byte, 2000)
+		for x := 0; x < len(board[y]); x++ {
+			board[y][x] = '#'
+		}
+	}
+	board[1000][1000] = 'X'
+
+	reader, err := os.Open("day20.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(reader)
+	if scanner.Scan() {
+		regex = scanner.Bytes()
+		regex = regex[1 : len(regex)-1]
+	}
+	reader.Close()
+
+	root := parseRegex(regex)
+	fmt.Println(root)
+	fmt.Println(root.traverse(500+500i, board))
+
+	for y := miny - 1; y <= maxy+1; y++ {
+		fmt.Println(string(board[y][minx-1 : maxx+2]))
+	}
+}
