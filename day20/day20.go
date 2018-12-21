47d8c6157f79aa33713d42bdf03791d1cf22c561
Jacob Wirth <xthexder@live.com>
2018-12-21 13:39:14 -0500

    Use [2]int instead of complex64

--- day20/day20.go
+++ day20/day20.go
@@ -7,11 +7,13 @@ import (
 	"os"
 )
 
+type Point [2]int
+
 var board [2000][2000]byte
 var distMap [1000][1000]int
 
-var minx, maxx int = 2000, 0
-var miny, maxy int = 2000, 0
+var minx, maxx int = 1000, 1000
+var miny, maxy int = 1000, 1000
 
 func minmax(x, y int) {
 	if x < minx {
@@ -99,42 +101,42 @@ func (n *Node) String() string {
 	return out
 }
 
-func (n *Node) traverse(pos complex64) []complex64 {
+func (n *Node) traverse(pos Point) []Point {
 	if n == nil {
-		return []complex64{pos}
+		return []Point{pos}
 	}
 
-	dist := distMap[int(imag(pos))][int(real(pos))]
+	dist := distMap[pos[1]][pos[0]]
 	for _, v := range n.value {
 		switch v {
 		case 'N':
-			board[int(imag(pos)*2)-1][int(real(pos)*2)] = '-'
-			pos += -1i
+			board[pos[1]*2-1][pos[0]*2] = '-'
+			pos[1]--
 		case 'S':
-			board[int(imag(pos)*2)+1][int(real(pos)*2)] = '-'
-			pos += 1i
+			board[pos[1]*2+1][pos[0]*2] = '-'
+			pos[1]++
 		case 'E':
-			board[int(imag(pos)*2)][int(real(pos)*2)+1] = '|'
-			pos += 1
+			board[pos[1]*2][pos[0]*2+1] = '|'
+			pos[0]++
 		case 'W':
-			board[int(imag(pos)*2)][int(real(pos)*2)-1] = '|'
-			pos += -1
+			board[pos[1]*2][pos[0]*2-1] = '|'
+			pos[0]--
 		default:
 			panic("Unknown direction")
 		}
 		dist++
-		board[int(imag(pos)*2)][int(real(pos)*2)] = '.'
-		minmax(int(real(pos)*2), int(imag(pos)*2))
-		if distMap[int(imag(pos))][int(real(pos))] == 0 || distMap[int(imag(pos))][int(real(pos))] > dist {
-			distMap[int(imag(pos))][int(real(pos))] = dist
+		board[pos[1]*2][pos[0]*2] = '.'
+		minmax(pos[0]*2, pos[1]*2)
+		if distMap[pos[1]][pos[0]] == 0 || distMap[pos[1]][pos[0]] > dist {
+			distMap[pos[1]][pos[0]] = dist
 		}
 	}
 	if len(n.children) == 0 {
 		return n.next.traverse(pos)
 	}
 
-	var ends []complex64
-	endSet := make(map[complex64]struct{})
+	var ends []Point
+	endSet := make(map[Point]struct{})
 	for _, child := range n.children {
 		childEnds := child.traverse(pos)
 		for _, end := range childEnds {
@@ -171,7 +173,7 @@ func main() {
 	root := parseRegex(regex)
 	// fmt.Println(root)
 
-	root.traverse(500 + 500i)
+	root.traverse(Point{500, 500})
 
 	max := 0
 	roomCount := 0
