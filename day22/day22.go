a886a02f606cab4553cc0a6460a7d38321f7ad78
Jacob Wirth <xthexder@live.com>
2018-12-21 20:16:58 -0500

    Fix earlier revision that's much faster

--- day22/day22.go
+++ day22/day22.go
@@ -17,7 +17,7 @@ const (
 	CLIMBING_GEAR
 )
 
-var tileChars = []string{".", "=", "|"}
+var tileChars = []string{".", "=", "|", "#"}
 var directions = [][2]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
 
 func erosion(index int) int {
@@ -26,22 +26,40 @@ func erosion(index int) int {
 
 type Point [2]int
 
-var weights [depth][depth][3]int
-
-type Entry [3]int // x, y, gear
-
-func (e Entry) add(a [2]int) [2]int {
-	return Point{e[0] + a[0], e[1] + a[1]}
+func (p Point) add(a Point) Point {
+	return Point{p[0] + a[0], p[1] + a[1]}
 }
 
-func (e Entry) Weight() *int {
-	return &weights[e[0]][e[1]][e[2]]
+var weights [depth][depth][3]int
+var geologicIndex [depth][depth]int
+var erosionLevel [depth][depth]int
+
+type Entry struct {
+	pos      Point
+	gear     int
+	previous *Entry
+}
+
+func (e *Entry) Weight() *int {
+	return &weights[e.pos[0]][e.pos[1]][e.gear]
+}
+
+func printBoard() {
+	for y := 0; y <= targetY+10; y++ {
+		for x := 0; x <= targetX+50; x++ {
+			if x == 0 && y == 0 {
+				fmt.Print("M")
+			} else if x == targetX && y == targetY {
+				fmt.Print("T")
+			} else {
+				fmt.Print(tileChars[erosionLevel[x][y]])
+			}
+		}
+		fmt.Println()
+	}
 }
 
 func main() {
-	var geologicIndex [depth][depth]int
-	var erosionLevel [depth][depth]int
-
 	for x := 0; x < depth; x++ {
 		geologicIndex[x][0] = x * 16807
 		erosionLevel[x][0] = erosion(geologicIndex[x][0])
@@ -64,66 +82,70 @@ func main() {
 			erosionLevel[x][y] %= 3
 		}
 	}
-	for y := 0; y <= targetY; y++ {
-		for x := 0; x <= targetX; x++ {
+	for x := 0; x <= targetX; x++ {
+		for y := 0; y <= targetY; y++ {
 			riskLevel += erosionLevel[x][y]
-			// if x == 0 && y == 0 {
-			// 	fmt.Print("M")
-			// } else if x == targetX && y == targetX {
-			// 	fmt.Print("T")
-			// } else {
-			// 	fmt.Print(tileChars[erosionLevel[x][y]])
-			// }
 		}
-		// fmt.Println()
 	}
 	fmt.Println("Part A:", riskLevel)
 
-	tiles := erosionLevel
-	visited := make(map[Entry]struct{})
-	unvisited := make(map[Entry]struct{})
-
-	visited[Entry{0, 0, TORCH}] = struct{}{}
-	unvisited[Entry{0, 0, TORCH}] = struct{}{}
 	weights[0][0][TORCH] = 1
+	tiles := erosionLevel
+	queue := make([]*Entry, 1, 10000)
+	queue[0] = &Entry{Point{0, 0}, TORCH, nil}
 
-	for len(unvisited) > 0 {
-		for entry, _ := range visited {
-			for _, dir := range directions {
-				pos := entry.add(dir)
-				if pos[0] < 0 || pos[1] < 0 {
-					continue
-				}
-				if entry[2] != tiles[pos[0]][pos[1]] {
-					// Don't need to change gear
-					posWeight := &weights[pos[0]][pos[1]][entry[2]]
-					if *posWeight == 0 || *posWeight > *entry.Weight()+1 {
-						*posWeight = *entry.Weight() + 1
-						unvisited[Entry{pos[0], pos[1], entry[2]}] = struct{}{}
-					}
-				}
-			}
-			gear := 3 ^ (entry[2] ^ tiles[entry[0]][entry[1]])
-			posWeight := &weights[entry[0]][entry[1]][gear]
-			if *posWeight == 0 || *posWeight > *entry.Weight()+7 {
-				*posWeight = *entry.Weight() + 7
-				unvisited[Entry{entry[0], entry[1], gear}] = struct{}{}
-			}
-		}
+	for len(queue) > 0 {
 		min := -1
-		for entry, _ := range unvisited {
+		for _, entry := range queue {
 			if min < 0 || *entry.Weight() < min {
 				min = *entry.Weight()
 			}
 		}
-		for entry, _ := range unvisited {
-			if *entry.Weight() == min {
-				if entry[0] == targetX && entry[1] == targetY && entry[2] == TORCH {
-					fmt.Println("Part B:", *entry.Weight()-1)
-					return
+		for {
+			startLen := len(queue)
+			for _, entry := range queue {
+				if *entry.Weight() == min {
+					if entry.pos[0] == targetX && entry.pos[1] == targetY && entry.gear == TORCH {
+						for e := entry; e != nil; e = e.previous {
+							erosionLevel[e.pos[0]][e.pos[1]] = 3
+						}
+						printBoard()
+						fmt.Println("Part B:", *entry.Weight()-1)
+						return
+					}
+
+					for _, dir := range directions {
+						pos := entry.pos.add(dir)
+						if pos[0] < 0 || pos[1] < 0 {
+							continue
+						}
+						if entry.gear != tiles[pos[0]][pos[1]] {
+							// Don't need to change gear
+							posWeight := &weights[pos[0]][pos[1]][entry.gear]
+							if *posWeight == 0 || *posWeight > *entry.Weight()+1 {
+								*posWeight = *entry.Weight() + 1
+								queue = append(queue, &Entry{pos, entry.gear, entry})
+							}
+						}
+					}
+					gear := 3 ^ (entry.gear ^ tiles[entry.pos[0]][entry.pos[1]])
+					posWeight := &weights[entry.pos[0]][entry.pos[1]][gear]
+					if *posWeight == 0 || *posWeight > *entry.Weight()+7 {
+						*posWeight = *entry.Weight() + 7
+						queue = append(queue, &Entry{entry.pos, gear, entry})
+					}
 				}
-				visited[entry] = struct{}{}
-				delete(unvisited, entry)
+			}
+			if len(queue) == startLen {
+				j := 0
+				for i, entry := range queue[:] {
+					if *entry.Weight() == min {
+						queue[j], queue[i] = queue[i], queue[j]
+						j++
+					}
+				}
+				queue = queue[j:]
+				break
 			}
 		}
 	}
